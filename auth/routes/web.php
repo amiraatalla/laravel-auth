6952e53f9251e33d140e6f7827d3ccadf86a60ff
Amira-Atallah <a.atallah@studio50s.com>
2022-01-09 23:02:37 +0200

    logout

--- auth/routes/web.php
+++ auth/routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
 
-Route::get('user', [AuthController::class, 'user']);
 Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class , 'login']);
+Route::post('login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [AuthController::class, 'user']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
